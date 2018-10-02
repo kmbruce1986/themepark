ae9f50765f49a3069e361fb61ee9fb3b53f6ae6b
kmbruce1986 <kmbruce1986@gmail.com>
2018-10-02 16:15:46 +0100

    initial commit

---
+++ src/main/java/Ride.java
@@ -0,0 +1,10 @@
+public abstract class Ride {
+    private double price;
+    private double minimumHeight;
+
+
+    public Ride(double price, double minimumHeight) {
+        this.price = price;
+        this.minimumHeight = minimumHeight;
+    }
+}
---
+++ src/test/java/RideTest.java
@@ -0,0 +1,11 @@
+import org.junit.Before;
+
+public class RideTest {
+
+    Ride ride;
+
+    @Before
+    public void setUp() {
+        ride = new Ride();
+    }
+}
